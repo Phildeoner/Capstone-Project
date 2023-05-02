d6cc2a532bbbf0795e05e4c9abb18be44d4e6d74
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-05-02 07:59:52 +0100

    Update style.css and create toggle.js and import to script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+import { toggleMenu } from './toggle.js';
+const menuBtn = document.querySelector('.menu-btn');
+
+menuBtn.addEventListener('click', toggleMenu);
+
 const hour = document.querySelector('.hour');
 const minute = document.querySelector('.minute');
 const second = document.querySelector('.second');
@@ -18,33 +23,6 @@ const updateTime = () => {
 };
 setInterval(updateTime, 1000);
 updateTime();
-
-const menuBtn = document.querySelector('.menu-btn');
-const reaction = document.querySelector('.menu-btn_options');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__item');
-let showMenu = false;
-
-function toggleMenu() {
-  if (!showMenu) {
-    reaction.classList.add('open');
-    nav.classList.add('open');
-    menuNav.classList.add('open');
-    navItems.forEach((item) => item.classList.add('open'));
-
-    showMenu = true;
-  } else {
-    reaction.classList.remove('open');
-    nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navItems.forEach((item) => item.classList.remove('open'));
-
-    showMenu = false;
-  }
-}
-
-menuBtn.addEventListener('click', toggleMenu);
 
 const data = [
   {
